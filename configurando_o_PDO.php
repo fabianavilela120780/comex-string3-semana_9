c12bcda847b5a3f787d19bbb4dacdc16bb4d6213
Fabiana Vilela <fabiana.vlmoura@sp.senac.br>
2023-11-14 17:24:23 -0300

    Update configurando_o_PDO.php

--- configurando_o_PDO.php
+++ configurando_o_PDO.php
@@ -1,11 +1,34 @@
-<php
-    $caminho = _DIR_ . " /../../db.sqlite";
-    
-    $pdo = new PDO('sqlite: ' . $caminho);
+<?php
 
-    $sql = "CREATE TABLE clientes (
-    id INTEGER PRIMARY KEY,
-    nome TEXT,
-    email TEXT,
-    celular TEXT,
-    );";    
+$caminhoBanco = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $caminhoBanco);
+
+echo 'Conectei';
+
+$createTableSql = "CREATE TABLE IF NOT EXISTS Cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    celular VARCHAR(15) NOT NULL,
+    endereco TEXT,
+    totalCompras DECIMAL(10, 2),
+    dataCadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Produto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    quantidadeEmEstoque INT NOT NULL,
+    dataCadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Pedido (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    clienteId INT NOT NULL,
+    dataPedido DATE,
+    valorTotal DECIMAL(10, 2),
+    status VARCHAR(50),
+    dataCadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (clienteId) REFERENCES Cliente(id)
+);";
